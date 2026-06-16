0022f384b3675ba8d7724dff5cf383bc6046f99f
Mohamed Maiga <mohamed.maiga@ism.edu.sn>
2026-06-16 13:54:15 +0000

    test

--- src/main/java/gesimmo/nekaso/config/CorsConfig.java
+++ src/main/java/gesimmo/nekaso/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
             .allowedOrigins(
                 "https://nekaso.me",
                 "http://localhost:5173",
