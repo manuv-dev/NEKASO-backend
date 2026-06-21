f7416e6b250c995270121f7f973c98094fc8e973
manuv-dev <komi-emmanuel.nyadzi@ism.edu.sn>
2026-06-21 18:31:08 +0000

    modification dashboard

--- src/main/java/gesimmo/nekaso/controller/DashboardController.java
+++ src/main/java/gesimmo/nekaso/controller/DashboardController.java
@@ -1,25 +1,26 @@
 package gesimmo.nekaso.controller;
 
 import gesimmo.nekaso.dto.DashboardResponseDTO;
+import gesimmo.nekaso.entity.Gestionnaire;
 import gesimmo.nekaso.service.DashboardService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/dashboard")
 @RequiredArgsConstructor
-@Tag(name = "Dashboard Management", description = "Endpoints de statistiques et d'analyse pour les tableaux de bord")
-@CrossOrigin(origins = "*") // Permet les appels cross-origin depuis ton front-end
 public class DashboardController {
 
     private final DashboardService dashboardService;
 
-    @GetMapping("/gestionnaire/{gestionnaireId}")
-    @Operation(summary = "Récupérer l'ensemble des données analytiques et KPIs du tableau de bord d'un gestionnaire")
-    public ResponseEntity<DashboardResponseDTO> getGestionnaireDashboard(@PathVariable Long gestionnaireId) {
+    @GetMapping("/gestionnaire")
+    public ResponseEntity<DashboardResponseDTO> getGestionnaireDashboard(Authentication authentication) {
+        Gestionnaire gestionnaire = (Gestionnaire) authentication.getPrincipal();
+        Long gestionnaireId = gestionnaire.getId();
         return ResponseEntity.ok(dashboardService.getGestionnaireDashboard(gestionnaireId));
     }
 }
